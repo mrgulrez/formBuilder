6a60513ddcc61132d61f747c3036d9afb7401952
Gulrez Alam <egulrezalam@gmail.com>
2024-03-22 02:52:21 +0530

    Refactor form builder migrations and update settings.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s*ipu&ecjl*b9qsr_e+b)%6fi*7+t5%ak1wl838fnxrt)s2$=q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-s*ipu&ecjl*b9qsr_e+b)%6fi*7+t5%ak1wl838fnxrt)s2$=q')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ovoting.onrender.com']
 
 
 # Application definition
@@ -75,14 +77,16 @@ WSGI_APPLICATION = "src.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "form_builder",
-        "USER": "root",
-        "PASSWORD": "Admin@123",
-        "HOST": "172.21.192.1",
-        "PORT": "3306",
+if not DEBUG:
+    DATABASES = {
+	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
